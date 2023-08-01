0c5d386c1c35dc48e7b4280b31e7c272af8e101f
Gusev Roman <tek822@yandex.ru>
2023-08-01 19:57:10 +0300

    fix: ConfirmedRequests.dto typo
    Took 10 minutes

--- ewm-service/src/main/java/ru/practicum/request/dto/EventRequestStatusUpdateResult.java
+++ ewm-service/src/main/java/ru/practicum/request/dto/EventRequestStatusUpdateResult.java
@@ -8,6 +8,6 @@ import java.util.List;
 @Data
 @AllArgsConstructor
 public class EventRequestStatusUpdateResult {
-    List<ParticipationRequestDto> confirmedRequest;
-    List<ParticipationRequestDto> rejectedRequest;
+    List<ParticipationRequestDto> confirmedRequests;
+    List<ParticipationRequestDto> rejectedRequests;
 }
